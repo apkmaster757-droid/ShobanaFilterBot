6f42ced5a178bac7221fc206a04d5c757cd4edde
apkmaster757-droid <apkmaster757@gmail.com>
2026-05-20 07:36:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,19 +13,10 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 AUTH_USERS = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_GROUPS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_GROUPS', '').split()]
 
-# 🗄️ Database Config (Up to 5 Clusters supported)
+# 🗄️ Database Config
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
-
-DATABASE_URI2 = environ.get('DATABASE_URI2', "")
-DATABASE_NAME2 = environ.get('Cluster2')
-DATABASE_URI3 = environ.get('DATABASE_URI3', "")
-DATABASE_NAME3 = environ.get('Cluster3')
-DATABASE_URI4 = environ.get('DATABASE_URI4', "")
-DATABASE_NAME4 = environ.get('Cluster4')
-DATABASE_URI5 = environ.get('DATABASE_URI5', "")
-DATABASE_NAME5 = environ.get('Cluster5')
 
 # 📢 Links & Channel Promotion Settings
 BRANDING_LINK = "https://t.me/Moviepornindia"
@@ -41,7 +32,7 @@ CUSTOM_FILE_CAPTION = environ.get('CUSTOM_FILE_CAPTION', "📁 <b>File Name:</b>
 BATCH_FILE_CAPTION = environ.get('BATCH_FILE_CAPTION', "📁 <b>File Name:</b> {file_name}\n\n⚙️ <b>Size:</b> {file_size}\n\n🍿 <b>Join Here:</b> @Moviepornindia")
 IMDB_TEMPLATE = environ.get('IMDB_TEMPLATE', "<b>Title:</b> {title}\n<b>Rating:</b> {rating}")
 
-# 🛠️ Feature Toggles & Extra Variables (Crucial Fixes)
+# 🛠️ Feature Toggles & Extra Variables
 P_TTI_SHOW_OFF = environ.get('P_TTI_SHOW_OFF', "False") == "True"
 IMDB = environ.get('IMDB', "False") == "True"
 SINGLE_BUTTON = environ.get('SINGLE_BUTTON', "True") == "True"
@@ -56,7 +47,7 @@ HYPER_MODE = environ.get('HYPER_MODE', "False") == "True"
 USE_CAPTION_FILTER = False
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-100'))
 
-# 🚨 All Missing Variables Required by commands.py & utils.py
+# 🚨 All Missing Variables Fix
 MELCOW_NEW_USERS = environ.get('MELCOW_NEW_USERS', "True") == "True"
 SESSION = environ.get('SESSION', 'Media_search')
 LOG_STR = environ.get('LOG_STR', '')
@@ -71,5 +62,6 @@ SHORTENER_WEBSITE = environ.get('SHORTENER_WEBSITE', "")
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 FILE_CHANNELS = [int(ch) for ch in environ.get('FILE_CHANNELS', '').split()]
 FILE_STORE_CHANNEL = [int(ch) for ch in environ.get('FILE_STORE_CHANNEL', '').split()]
+FILE_CHANNEL_SENDING_MODE = environ.get('FILE_CHANNEL_SENDING_MODE', '0')
 FILE_CHANNEL_SEND_MODE = environ.get('FILE_CHANNEL_SEND_MODE', '0')
 FILE_AUTO_DELETE_SECONDS = int(environ.get('FILE_AUTO_DELETE_SECONDS', '0'))
