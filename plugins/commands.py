02e59890f43c01962ac002d0a76147b99caf8e60
apkmaster757-droid <apkmaster757@gmail.com>
2026-05-20 10:26:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -213,12 +213,12 @@ async def start(client, message):
         m=await message.reply_text("ShobanaFilterBot") 
         await asyncio.sleep(1.2)
         await m.delete()
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+                await message.reply_text(
+            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-        )
+            parse_mode=enums.ParseMode.HTML,
+            disable_web_page_preview=True
+                )
         return
     if len(message.command) == 2 and message.command[1].startswith('mntgx'):
         payload = message.command[1]
